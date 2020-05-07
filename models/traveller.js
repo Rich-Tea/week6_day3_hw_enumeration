79bd56176f3233009ec3fdebc7ed4c97df183109
Rich <richgrant192@gmail.com>
2020-05-07 23:30:10 +0100

    initial commit

---
+++ models/traveller.js
@@ -0,0 +1,42 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === "train";
+  });
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= 1000;
+  });
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+     return total += journey.distance;
+  }, 0)
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+   uniqueTransport = this.journeys.map(journey => journey.transport);
+   return uniqueTransport.filter((transport, index, map) => map.indexOf(transport) === index);
+   
+};
+
+
+module.exports = Traveller;
